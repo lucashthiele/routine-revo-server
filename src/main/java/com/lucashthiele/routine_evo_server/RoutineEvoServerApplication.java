af4393aeef6cd5fa16e292d0cf988dd64c4784ce
lucashthiele <lucasthiele0@outlook.com>
2025-10-28 22:00:24 -0300

    init

---
+++ src/main/java/com/lucashthiele/routine_evo_server/RoutineEvoServerApplication.java
@@ -0,0 +1,13 @@
+package com.lucashthiele.routine_evo_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineEvoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutineEvoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucashthiele/routine_evo_server/RoutineEvoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucashthiele.routine_evo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineEvoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
